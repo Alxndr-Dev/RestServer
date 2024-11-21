31b534db0e4632d47bc158ef8d875baf17ff1efc
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-11-20 22:20:55 -0400

    Separating the routes and controller from the class

--- models/server.js
+++ models/server.js
@@ -5,6 +5,7 @@ class Server {
     constructor(){
         this.app = express();
         this.port = process.env.PORT;
+        this.userPath = '/api/users';
 
         //Middlewares
         this.middlewares();
@@ -23,35 +24,9 @@ class Server {
     }
 
     routes(){
-        this.app.get('/api', (req, res) => {
-            res.json({
-                msg: 'get API'
-            });
-        });
+        
+        this.app.use(this.userPath, require('../routes/user.routes'));
 
-        this.app.put('/api', (req, res) => {
-            res.status(500).json({
-                msg: 'put API'
-            });
-        });
-
-        this.app.post('/api', (req, res) => {
-            res.status(201).json({
-                msg: 'post API'
-            });
-        });
-
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                msg: 'delete API'
-            });
-        });
-
-        this.app.patch('/api', (req, res) => {
-            res.json({
-                msg: 'patch API'
-            });
-        });
     }
 
     listen(){
