0cd4ac56fa444391597b98ae880f6cfac75945bf
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-11-28 23:36:02 -0400

    Fields Validating

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,6 +7,7 @@ const {getUsers,
     putUsers, 
     patchUsers,
     deleteUsers} = require('../controllers/user.controller');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 
 
@@ -14,6 +15,10 @@ router.get('/', getUsers);
 
 router.post('/', [
     check('correo', 'El correo no es valido').isEmail(),
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe de ser mas de 6 letras').isLength({min: 6}),
+    check('role', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
 ] ,postUsers);
 
 router.put('/:id', putUsers);
