8e6afd16162b5487fca9883b6bfd50276e90829d
Alexander Sanchez <sanchezalexander2000@gmail.com>
2024-11-29 23:54:55 -0400

    Role Validation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,6 +8,7 @@ const {getUsers,
     patchUsers,
     deleteUsers} = require('../controllers/user.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
+const Role = require('../models/role');
 
 
 
@@ -17,7 +18,13 @@ router.post('/', [
     check('correo', 'El correo no es valido').isEmail(),
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe de ser mas de 6 letras').isLength({min: 6}),
-    check('role', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('role', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom( async (role = '') =>{
+        const existeRol = await Role.findOne({role});
+        if(!existeRol){
+            throw new Error(`El rol ${role} no esta registrado en la Base de datos`);
+        }
+    }),
     validarCampos
 ] ,postUsers);
 
